540f5f4169338a767cb92af8dfbb9cf887a3b8a6
Stanislav Stehniy <sstehniy@gmail.com>
2023-06-02 22:24:26 +0100

    implement the algorithm

--- src/main.rs
+++ src/main.rs
@@ -23,22 +23,34 @@ fn vigenere() -> String {
             .unwrap();
         tableau[i] = shifted_alphabet;
     }
+    for i in 0..tableau.len() {
+        println!("{:?}", tableau[i]);
+    }
+    let mut operation_type: i8;
 
-    println!("{:?}", tableau.iter().find(|x| x[0] == 'h').unwrap());
+    loop {
+        println!("Choose what operation you want to perform:");
+        println!("1. Encrypt text with a key");
+        println!("2. Decrypt text with a key");
+        let mut operation_input: String = String::new();
 
-    println!("Choose what operation you want to perform:");
-    println!("1. Encrypt text with a key");
-    println!("2. Decrypt text with a key");
+        io::stdin()
+            .read_line(&mut operation_input)
+            .expect("Failed to read line");
 
-    let mut operationInput = String::new();
-    io::stdin()
-        .read_line(&mut operationInput)
-        .expect("Failed to read line");
+        operation_type = match operation_input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => 0,
+        };
 
-    let operationType: i8 = match operationInput.trim().parse() {
-        Ok(num) => num,
-        Err(_) => 0,
-    };
+        println!("Chosen option: {}", operation_type);
+
+        if operation_type < 1 || operation_type > 2 {
+            println!("Wrong input: please enter either 1 or 2 and press return");
+        } else {
+            break;
+        }
+    }
 
     let mut input_key = String::new();
     let mut input_phrase = String::new();
@@ -57,8 +69,81 @@ fn vigenere() -> String {
 
     let key_chars = key.chars().collect::<Vec<char>>();
     let phrase_chars = phrase.chars().collect::<Vec<char>>();
-    let key_queue = key_chars.iter().cycle();
-    for ch in 0..phrase_chars.len() {}
+
+    if operation_type == 1 {
+        result = perform_encrypt(&alphabet_chars, &tableau, &key_chars, &phrase_chars);
+    } else if operation_type == 2 {
+        result = perform_decrypt(&alphabet_chars, &tableau, &key_chars, &phrase_chars);
+    }
+    println!(
+        "Convertion of phrase {} with key {}: {}",
+        input_phrase, input_key, result
+    );
+    return result;
+}
+
+fn perform_encrypt(
+    alphabet_chars: &[char; 26],
+    tableau: &[[char; 26]; 26],
+    key_chars: &Vec<char>,
+    phrase_chars: &Vec<char>,
+) -> String {
+    let mut result = String::new();
+    let mut cycl = key_chars.iter().cycle();
+
+    for i in 0..phrase_chars.len() {
+        if !alphabet_chars.contains(&phrase_chars[i]) {
+            result.push(phrase_chars[i]);
+            continue;
+        }
+        let current_key_char = cycl.next().unwrap();
+        let current_phrase_char = &phrase_chars[i];
+        let current_key_char_index_option =
+            alphabet_chars.iter().position(|x| x == current_key_char);
+
+        let current_phrase_char_index_option =
+            alphabet_chars.iter().position(|x| x == current_phrase_char);
+        if current_key_char_index_option.is_none() || current_phrase_char_index_option.is_none() {
+            continue;
+        }
+        // println!("key char: {}, phrase char: {}, key alpha index: {}, phase alpha index: {}, tablue row: {:?}", current_key_char, current_phrase_char,current_key_char_index_option.unwrap(), current_phrase_char_index_option.unwrap(), tableau[current_phrase_char_index_option.unwrap()]);
+        let curr_position_char = tableau[current_phrase_char_index_option.unwrap()]
+            [current_key_char_index_option.unwrap()];
+        result.push(curr_position_char);
+    }
+
+    return result;
+}
+
+fn perform_decrypt(
+    alphabet_chars: &[char; 26],
+    tableau: &[[char; 26]; 26],
+    key_chars: &Vec<char>,
+    phrase_chars: &Vec<char>,
+) -> String {
+    let mut result = String::new();
+    let mut cycl = key_chars.iter().cycle();
+
+    for i in 0..phrase_chars.len() {
+        if !alphabet_chars.contains(&phrase_chars[i]) {
+            result.push(phrase_chars[i]);
+            continue;
+        }
+        let current_key_char = cycl.next().unwrap();
+        let current_phrase_char = &phrase_chars[i];
+        let current_key_char_index_option =
+            alphabet_chars.iter().position(|x| x == current_key_char);
+
+        let current_phrase_char_index_option =
+            alphabet_chars.iter().position(|x| x == current_phrase_char);
+        if current_key_char_index_option.is_none() || current_phrase_char_index_option.is_none() {
+            continue;
+        }
+        println!("key char: {}, phrase char: {}, key alpha index: {}, phase alpha index: {}, tablue row: {:?}", current_key_char, current_phrase_char,current_key_char_index_option.unwrap(), current_phrase_char_index_option.unwrap(), tableau[current_phrase_char_index_option.unwrap()]);
+        let curr_position_char = tableau[current_phrase_char_index_option.unwrap()]
+            [alphabet_chars.len() - current_key_char_index_option.unwrap()];
+        result.push(curr_position_char);
+    }
 
     return result;
 }
